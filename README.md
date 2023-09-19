fccbfa55dde5fe3554ad2f11b63a280101934778
Michael Paulukonis <michael.paulukonis@gmail.com>
2023-09-19 00:41:45 -0400

    Tweaks notes etc also draw on file load

--- README.md
+++ README.md
@@ -5,3 +5,12 @@ check out the blog and live demo here:
 https://rangdo.github.io/jekyll/update/2018/09/25/speccy-vision.html
 
 
+## Break apart code and UI
+
+Playing with it.
+
+
+There's a bit of p5js in it
+And all images are reset to size 256x192
+
+
--- speccy.js
+++ speccy.js
@@ -1,22 +1,22 @@
 // Experiments in automatically converting images into a image resembling a zx specrum screen
 
 var speccy_palette = [
-    [0, 0, 0],      // black
-    [215, 0, 0],    // dark red
-    [0, 215, 0],    // dark green
-    [215, 215, 0],  // dark yellow
-    [0, 0, 215],    // dark blue
-    [215, 0, 215],  // dark magenta
-    [0, 215, 215],  // dark cyan
-    [0, 0, 0],      // black
-    [215, 215, 215], // grey
-    [255, 0, 0],     // red
-    [0, 255, 0],     // green
-    [255, 255, 0],   // yellow
-    [0, 0, 255],     // blue
-    [255, 0, 255],   // magenta
-    [0, 255, 255],   // cyan
-    [255, 255, 255]  // white
+	[0, 0, 0],      // black
+	[215, 0, 0],    // dark red
+	[0, 215, 0],    // dark green
+	[215, 215, 0],  // dark yellow
+	[0, 0, 215],    // dark blue
+	[215, 0, 215],  // dark magenta
+	[0, 215, 215],  // dark cyan
+	[0, 0, 0],      // black
+	[215, 215, 215], // grey
+	[255, 0, 0],     // red
+	[0, 255, 0],     // green
+	[255, 255, 0],   // yellow
+	[0, 0, 255],     // blue
+	[255, 0, 255],   // magenta
+	[0, 255, 255],   // cyan
+	[255, 255, 255]  // white
 ];
 
 // subtract two arrays
@@ -25,8 +25,8 @@ function sub(a, b) {
 }
 
 // dot product two arrays
-function dot(a,b) {
-	let n = 0, lim = Math.min(a.length,b.length);
+function dot(a, b) {
+	let n = 0, lim = Math.min(a.length, b.length);
 	for (var i = 0; i < lim; i++) n += a[i] * b[i];
 	return n;
 }
@@ -43,7 +43,7 @@ function rgb2speccy(r, g, b, palette, start, end) {
 	let best_color = start;
 	let best_distance_sqrt = vdist(palette[start], [r, g, b]);
 
-	for (var i = start+1; i < end; i++) {
+	for (var i = start + 1; i < end; i++) {
 		let d = vdist(palette[i], [r, g, b]);
 		if (d < best_distance_sqrt) {
 			best_distance_sqrt = d;
@@ -55,21 +55,20 @@ function rgb2speccy(r, g, b, palette, start, end) {
 }
 
 const rgb2light = (r, g, b) => rgb2speccy(r, g, b, speccy_palette, 0, 9);
-const rgb2dark  = (r, g, b) => rgb2speccy(r, g, b, speccy_palette, 9, speccy_palette.length);
+const rgb2dark = (r, g, b) => rgb2speccy(r, g, b, speccy_palette, 9, speccy_palette.length);
 
 // 0.0 - 1.0 rgb
-function rgb2hsl(r, g, b)
-{
+function rgb2hsl(r, g, b) {
 	const min = Math.min(r, g, b);
 	const max = Math.max(r, g, b);
 
-    const light = (min + max) / 2.0;
-    let sat = 0.0;
+	const light = (min + max) / 2.0;
+	let sat = 0.0;
 
-	if( min == max ) {
+	if (min == max) {
 		return [-1, light, sat];
 	}
-	
+
 	const delta = max - min;
 
 	if (light <= 0.5) {
@@ -79,23 +78,23 @@ function rgb2hsl(r, g, b)
 		sat = delta / (2.0 - (max + min));
 	}
 
-    let hue = 0.0;
+	let hue = 0.0;
 
-    if (r == max) {
-        hue = (g - b) / delta;		// between yellow & magenta
-    }
-    else if (g == max) {
-        hue = 2.0 + (b - r) / delta;	// between cyan & yellow
-    }
-    else {
-        hue = 4.0 + (r - g) / delta;	// between magenta & cyan
-    }
+	if (r == max) {
+		hue = (g - b) / delta;		// between yellow & magenta
+	}
+	else if (g == max) {
+		hue = 2.0 + (b - r) / delta;	// between cyan & yellow
+	}
+	else {
+		hue = 4.0 + (r - g) / delta;	// between magenta & cyan
+	}
 
-    hue *= 60.0;				// degrees
-    
-    if (hue < 0.0) {
-        hue += 360.0;
-    }
+	hue *= 60.0;				// degrees
+
+	if (hue < 0.0) {
+		hue += 360.0;
+	}
 
 	return [hue, light, sat];
 }
@@ -103,24 +102,12 @@ function rgb2hsl(r, g, b)
 function hsl2speccydark(hue, light, sat) {
 
 	if (sat > 0.25 && light > 0.25 && hue >= 0) {
-		if (hue > 270) {
-			return 5; // magenta
-		}
-		else if (hue > 210) {
-			return 4; // blue
-		}
-		else if (hue > 150) {
-			return 6; // cyan
-		}
-		else if (hue > 90) {
-			return 2;   // green
-		}
-		else if (hue > 30) {
-			return 3; //yellow
-		}
-		else if (hue >= 0) {
-			return 1; // red
-		}
+		if (hue > 270) return 5; // magenta
+		if (hue > 210) return 4; // blue
+		if (hue > 150) return 6; // cyan
+		if (hue > 90) return 2;   // green
+		if (hue > 30) return 3; //yellow
+		if (hue >= 0) return 1; // red
 	}
 	else {
 		return 0;
@@ -152,32 +139,32 @@ function hsl2speccylight(hue, light, sat) {
 		// map to a speccy grey
 		let i = 8;
 		if (light >= 0.84) i = 15;
-		
+
 		return i;
 	}
 }
 
 function rgb2hsl2light(r, g, b) {
 	let [hue, light, sat] = rgb2hsl(float(r) / 256.0,
-									float(g) / 256.0,
-									float(b) / 256.0);
-				
+		float(g) / 256.0,
+		float(b) / 256.0);
+
 	return hsl2speccylight(hue, light, sat);
 }
 
 function rgb2hsl2dark(r, g, b) {
 	let [hue, light, sat] = rgb2hsl(float(r) / 256.0,
-									float(g) / 256.0,
-									float(b) / 256.0);
-				
+		float(g) / 256.0,
+		float(b) / 256.0);
+
 	return hsl2speccydark(hue, light, sat);
 }
 
 var dither_matrix = [
-    [0,  8,  2,  10],
-    [12, 4,  14, 6 ],
-    [3,  11, 1,  9 ], 
-    [15, 7,  13, 5 ]
+	[0, 8, 2, 10],
+	[12, 4, 14, 6],
+	[3, 11, 1, 9],
+	[15, 7, 13, 5]
 ];
 
 let colorizeCells = true;
@@ -199,11 +186,11 @@ function dither(pixel, c, r) {
 function pix2block(c, r) {
 	const x = int(c / 8);
 	const y = int(r / 8);
-	return y * 32 + x;
+	return y * 32 * scaleFactor + x;
 }
 
 function createBlockBins() {
-	let blocks = new Array(32 * 24);
+	let blocks = new Array((32 * scaleFactor) * (24 * scaleFactor));  // 32 * 16 x 24 * 16 => 512 x 384 image
 
 	for (let i = 0; i < blocks.length; i++) {
 		blocks[i] = new Array(16).fill(0);
@@ -214,17 +201,19 @@ function createBlockBins() {
 
 // convert an image into a zx spectrum image
 // via binning
+// pix2block fails when img_in is bigger than the original size
+// that is, 768 and above
 function convertImage(img_in, img_out, lightFunc, darkFunc) {
 
 	let lblocks = lightFunc ? createBlockBins() : null;
 	let dblocks = darkFunc ? createBlockBins() : null;
 
-	for (var y = 0; y < img_in.height; y++) {
-		for (var x = 0; x < img_in.width; x++) {
+	for (let y = 0; y < img_in.height; y++) {
+		for (let x = 0; x < img_in.width; x++) {
 			const offset = pix2block(x, y);
 			const p = img_in.get(x, y);
 			const shade = dither(p, x, y);
-			
+
 			if (shade === 0) { // dark pixel
 				if (dblocks !== null && darkFunc !== null) {
 					let c = darkFunc(p[0], p[1], p[2]);
@@ -264,10 +253,8 @@ function convertImage(img_in, img_out, lightFunc, darkFunc) {
 
 function processImageDither(img_in, img_out) {
 
-    for(var r = 0; r < img_in.height; r++)
-	{
-		for(var c = 0; c < img_in.width; c++)
-		{
+	for (let r = 0; r < img_in.height; r++) {
+		for (let c = 0; c < img_in.width; c++) {
 			const p = img_in.get(c, r);
 
 			const shade = dither(p, c, r);
@@ -287,46 +274,41 @@ let backgroundColor = 'CORNFLOWERBLUE';
 let slider;
 let dirty = false;
 let dropdown;
+const scaleFactor = 2; // scale of output makes for a looooot of processing ouch
 
 function setup() {
-	const scaleFactor = 2;
 	canvas = createCanvas(256 * scaleFactor, 192 * scaleFactor);
 	canvas.parent('canvas-holder');
-    canvas.drop(gotFile);
+	canvas.drop(gotFile);
 
-    img_in = createImage(256, 192);
-	img_out = createImage(256, 192);
-	
+	img_in = createImage(256 * scaleFactor, 192 * scaleFactor);
+	img_out = createImage(256 * scaleFactor, 192 * scaleFactor);
+
 	slider = select("#start");
 	checkbox = select('#ditheronly', false);
 
 	dropdown = createSelect(); // or create dropdown?
-	dropdown.option('RGB','rgb');
-	dropdown.option('RGB Fixed','rgbfixed');
+	dropdown.option('RGB', 'rgb');
+	dropdown.option('RGB Fixed', 'rgbfixed');
 	dropdown.option('HSL', 'hsl');
 	dropdown.option('HSL Fixed', 'hslfixed');
 }
 
-// handle a drag and drop event to set the current image
 function gotFile(file) {
+	if (file.type !== 'image') {
+		console.log('not an image!');
+		return
+	}
+	fileURL = file.name;
 
-    // If it's an image file
-	if (file.type === 'image') {
-		
-		fileURL = file.name;
+	// convert the dom image data to a p5.js image
+	var raw = new Image();
+	raw.src = file.data;
+	raw.onload = () => {
+		// resize and render the image into the destination image for processing
+		img_in.drawingContext.drawImage(raw, 0, 0, 256 * scaleFactor, 192 * scaleFactor);
 		dirty = true;
-
-		// convert the dom image data to a p5.js image
-		var raw = new Image();
-		raw.src = file.data;
-		raw.onload = function () {
-			// resize and render the image into the destination image for processing
-			img_in.drawingContext.drawImage(raw, 0, 0, 256, 192);
-		}
-        
-    } else {
-      console.log('not an image!');
-    }
+	}
 }
 
 function draw() {
@@ -369,10 +351,17 @@ function draw() {
 	}
 
 	fill(255);
-  	noStroke();
-  	textSize(18);
-  	textAlign(CENTER);
+	noStroke();
+	textSize(18);
+	textAlign(CENTER);
 	text('Drag an image file here', width / 2, height / 2);
-    
-	image(img_out, 0, 0, img_out.width * 2, img_out.height * 2);
+
+	image(img_out, 0, 0, img_out.width, img_out.height);
+}
+
+function keyPressed() {
+	if (keyCode === 83) {
+		// Press 'S' to save
+		save(`speccy.${datestring()}.png`)
+	}
 }
